29de6cc068f336c2ebcca5c0286f621be4c161b2
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-24 12:02:49 +0700

    cart item

---
+++ ecommerce_api/lib/models/cart_item.dart
@@ -0,0 +1,17 @@
+class CartItem {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final String size;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.size,
+    required this.quantity,
+    required this.price,
+  });
+}

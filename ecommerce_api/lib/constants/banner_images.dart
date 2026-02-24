fffd2b5721fc7b34c6e0d31f564384a6e9d4f1a7
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-02-24 11:58:16 +0700

    Add images static banner

---
+++ ecommerce_api/lib/constants/banner_images.dart
@@ -0,0 +1,17 @@
+/* This images show banner statics */
+class BannerImages {
+  static const List<String> banners = [
+    /* image banner use link from network */
+    // 'https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?w=800&q=80',
+    // 'https://images.unsplash.com/photo-1441986300917-64674bd600d8?w=800&q=80',
+    // 'https://images.unsplash.com/photo-1483985988355-763728e1935b?w=800&q=80',
+    // 'https://images.unsplash.com/photo-1490481651871-ab68de25d43d?w=800&q=80',
+    // 'https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?w=800&q=80',
+
+    /* From folder image banner */
+    'assets/banner/Banner1.png',
+    'assets/banner/Banner2.png',
+    'assets/banner/Banner3.png',
+    'assets/banner/Banner4.png',
+  ];
+}
